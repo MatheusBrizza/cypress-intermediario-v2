b45c6d45560b6ae6dc3ca64b8cecb54dffa700d2
Matheus <matheusbj95@gmail.com>
2023-06-27 04:06:13 -0300

    feito exercício aula 2

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,10 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Página GitLab', () => {
+  beforeEach(function() {
+    cy.visit('http://localhost/users/sign_in')
+  })
+  it('fazer login inserindo usuário e senha', () => {
+    cy.get('#user_login').type('root')
+    cy.get('#user_password').type('leonkennedy')
+    cy.get('#new_user > .submit-container > .btn').click()
   })
 })
