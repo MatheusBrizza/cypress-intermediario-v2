f0c5d4e0d207d97f330c2764a6873663fbde253c
Matheus <matheusbj95@gmail.com>
2023-06-28 02:21:07 -0300

    corrigido exercício aula 3

--- cypress/e2e/gui/logout.cy.js
+++ cypress/e2e/gui/logout.cy.js
@@ -2,8 +2,8 @@ describe('logout GitLab', () => {
     beforeEach(function() {
       cy.login()
     })
-    it('fazer logout abrindo o menu de usuário no canto superior direito e pressionando o botão de sign out', () => {
+    it('fazer logout clicando no avatar e clicando no botão sign out', () => {
       cy.logout()
-      cy.get("[data-qa-selector='login_field']").should('be.visible')
+      cy.url().should('be.equal', `${Cypress.config('baseUrl')}/users/sign_in`)
     })
   })
