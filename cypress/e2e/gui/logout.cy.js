1b3b9aedb8333ec988125e7ee5c93a65b589d40d
Matheus <matheusbj95@gmail.com>
2023-07-04 04:00:53 -0300

    corrigido exercicio 4 aula 4

--- cypress/e2e/gui/logout.cy.js
+++ cypress/e2e/gui/logout.cy.js
@@ -1,6 +1,7 @@
 describe('logout GitLab', () => {
     beforeEach(function() {
       cy.login()
+      cy.visit('/')
     })
     it('fazer logout clicando no avatar e clicando no botão sign out', () => {
       cy.logout()
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -3,7 +3,8 @@
 // no campo de password foi adicionado o log: false para que a senha não vaze nos logs.  
 Cypress.Commands.add('login', (
     user = Cypress.env('user_name'),
-    password = Cypress.env('user_password'), 
+    password = Cypress.env('user_password'),
+    { cacheSession = true } = {}, 
   ) => {
     const login = () => {
       cy.visit('/users/sign_in')
@@ -11,9 +12,26 @@ Cypress.Commands.add('login', (
       cy.get("[data-qa-selector='login_field']").type(user)
       cy.get("[data-qa-selector='password_field']").type(password, { log: false })
       cy.get("[data-qa-selector='sign_in_button']").click()
+    
+    }
+
+    const validarSessao = () => {
+      cy.visit('/')
+      cy.location('pathname', { timeout: 1000 })
+        .should('not.eq', '/users/sign_in')
+    }
+
+    const opcoes = {
+      cacheAcrossSpecs: true,
+      validarSessao,
     }
   
-    login()
+    if (cacheSession) {
+      cy.session(user, login, opcoes)
+    } else {
+      login()
+    }
+  
   })
 
 Cypress.Commands.add('logout', () => {
